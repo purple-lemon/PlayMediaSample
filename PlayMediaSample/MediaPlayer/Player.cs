e376748f5e350573b746b65bd9594b2de474b434
byurch <byurchak@gmail.com>
2016-11-11 13:25:34 +0200

    Why am I doing it:)

---
+++ PlayMediaSample/MediaPlayer/Player.cs
@@ -0,0 +1,61 @@
+﻿using Accord.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PlayerLib
+{
+	public class Player
+	{
+		public void PlayAvi(string path, bool saveScreen = false)
+		{
+			try
+			{
+				var s = new VideoFileSource(path);
+				var reader = new VideoFileReader();
+				var uri = new Uri(path);
+				reader.Open(uri.AbsolutePath);
+				int framesToSkip = reader.FrameRate * 3600;
+				var i = 0;
+
+				while (true)
+				{
+					var screen = reader.ReadVideoFrame();
+					Console.WriteLine(i);
+					if (i % 25 == 0)
+					{
+						screen.Save(@"screen_" + i.ToString() + ".jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+					}
+					i++;
+				}
+			}
+			catch (Exception e)
+			{
+
+			}
+			finally
+			{
+			}
+		}
+
+		public void MPPlayAvi(string path, bool saveScreen = false)
+		{
+			try
+			{
+				var mp = new MediaPlayer();
+				mp.Open(new Uri(path));
+				var d = mp.NaturalDuration;
+			}
+			catch (Exception e)
+			{
+
+			}
+		}
+	}
+}
---
+++ PlayMediaSample/PlayMediaSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using PlayerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayMediaSample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var p = new Player();
+			p.MPPlayAvi(@"F:\Distr\Interactions\2016101919312339601397088885502201.avi");
+			p.MPPlayAvi(@"F:\Distr\Interactions\2016101223101001285500100000001002.wav");
+		}
+	}
+}
